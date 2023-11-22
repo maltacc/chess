8e5711262677de1e662ebfce0a2b0adfaa0126d6
maggieguo <m5guo@uwaterloo.ca>
2023-11-21 22:35:19 -0500

    Updated Square

--- square.h
+++ square.h
@@ -2,11 +2,9 @@
 #define __SQUARE_H__
 #include "piece.h"
 #include "move.h"
-#include <vector>
 
 class Square {
     Piece *p = nullptr; 
-    vector<Move> validMoves; // tracks the valid moves for every square
 
     public: 
         Square();
@@ -15,7 +13,6 @@ class Square {
         void setEmpty(); // remove piece from square
         bool isEmpty(); // square doesn't contain a piece
         void addPiece(char piece); // add piece to square
-        vector<Move> getMoves(); // get valid moves
         ~Square(); 
 }; 
 
