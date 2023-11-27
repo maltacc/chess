64df1fa52dcbcd8d6315582981b9f63de493016f
maggieguo <m5guo@uwaterloo.ca>
2023-11-27 11:16:28 -0500

    Updated main loop

--- main.cc
+++ main.cc
@@ -10,34 +10,32 @@ int main() {
     try {
         while (cin >> cmd) {
             if (cmd == "setup") {
-                if (b.getState() == State::InPlay) {
-                    cout << "Game is running. Stop first" << endl;
-                    break;
-                } 
-                while (cmd != "done") { // while in setup mode
-                    if (cmd == "+") {
-                        char piece; 
-                        string pos; 
-                        cin >> piece >> pos;
-                        if (b.getTurn() == Side::W) b.place(Piece{piece, Side::W}, Pos{pos});
-                        else b.place(Piece{piece, Side::B}, Pos{pos});
+                if (b.getState() == State::InPlay) cout << "Game is running. Stop first" << endl;
+                else {
+                    while (cmd != "done") { // while in setup mode
+                        if (cmd == "+") {
+                            char piece; 
+                            string pos; 
+                            cin >> piece >> pos;
+                            if (b.getTurn() == Side::W) b.place(Piece{piece, Side::W}, Pos{pos});
+                            else b.place(Piece{piece, Side::B}, Pos{pos});
+                        }
+                        else if (cmd == "-") {
+                            string pos; 
+                            cin >> pos; 
+                            b.remove(Pos{pos}); // remove piece from pos
+                        }
+                        else if (cmd == "=") {
+                            string side; 
+                            cin >> side; 
+                            if (side == "white") b.setTurn(Side::W); 
+                            else b.setTurn(Side::B); 
+                        }
                     }
-                    else if (cmd == "-") {
-                        string pos; 
-                        cin >> pos; 
-                        b.remove(Pos{pos}); // remove piece from pos
-                    }
-                    else if (cmd == "=") {
-                        string side; 
-                        cin >> side; 
-                        if (side == "white") b.setTurn(Side::W); 
-                        else b.setTurn(Side::B); 
-                    }
+                    // check the board contains 1 white king, 1 black king, no pawns 
+                    // on the first and last rank and that neither king is in check
+                    // b.isValidPosition()
                 }
-
-                // check the board contains 1 white king, 1 black king, no pawns 
-                // on the first and last rank and that neither king is in check
-                // isValidPosition()
             }
             else if (cmd == "game") {
                 string white, black; 
